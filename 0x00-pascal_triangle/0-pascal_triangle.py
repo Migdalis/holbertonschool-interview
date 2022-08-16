a3e791852d241a51082df99e19b7045144a6ff8c
Migdalis <miglepage@gmail.com>
2022-08-16 06:25:43 -0500

    :white_check_mark: Add task and Readme

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Pascal's triangle """
+
+
+def pascal_triangle(n):
+    """ Function that returns a list of lists of integers representing the Pascal’s triangle of n """
+    pas_trngl = []
+    if n <= 0:
+        return pas_trngl
+
+    pas_trngl.append([1])
+    for i in range(1, n):
+        row_trngl = [1]
+        for j in range(1, i):
+            row_trngl.append(pas_trngl[i - 1][j - 1] + pas_trngl[i - 1][j])
+        row_trngl.append(1)
+        pas_trngl.append(row_trngl)
+
+    return pas_trngl
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,5 @@
+Pascal’s Triangle is a method to know
+the binomial coefficients of terms of binomial expression (x + y)n,
+where n can be any positive integer
+and x,y are real numbers.
+This code make a pascal triangle give an integer
