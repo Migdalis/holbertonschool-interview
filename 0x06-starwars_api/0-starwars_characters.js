69512451302592fdc3182fd84a779b5595817e04
Migdalis <miglepage@gmail.com>
2022-09-08 23:32:59 -0500

    Fix task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,18 +4,18 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, async (err, resp, body) => {
-    if (err) {
-        console.log(err);
-    }
-    for (const character of JSON.parse(body).characters) {
-        await new Promise((resolve, reject) => {
-            request(character, (err, resp, body) => {
-                if (err) {
-                    reject(err);
-                }
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+  if (err) {
+    console.log(err);
+  }
+  for (const character of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, resp, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
